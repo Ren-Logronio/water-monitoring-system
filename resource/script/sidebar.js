3322284182c1c8209393c8a41704cdf346216260
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2023-07-12 04:54:58 +0800

    sidebar

--- resource/script/sidebar.js
+++ resource/script/sidebar.js
@@ -1,81 +1,126 @@
-const sidebarElement = document.querySelector("#sidebar");
-const sidebarCoverElement = document.querySelector("#sidebar-cover");
-const windowMatchMedia = window.matchMedia("(max-width: 768px)");
-const display = {
-    isSmall: () => { return windowMatchMedia.matches; }
-}
-let sidebar;
 
-class Sidebar {
-    constructor(sidebarElement, sidebarCoverElement) {
-        this.sidebarElement = sidebarElement;
-        this.sidebarCoverElement = sidebarCoverElement;
-        this.sidebarIsOpen = true;
-        this.initialize();
-    }
+    const sidebarElementId = "#sidebar";
+    const sidebarCoverElementId = "#sidebar-cover";
+    const sidebarToggleElementId = "#sidebar-toggle";
+    const smallDisplayWidth = "768px";
+    const Animation = { open : true, close : false}
 
-    // ALL THE CODE NEEDED TO UNDERSTAND SIDEBAR FUNCTIONALITY
+    let sidebar;
 
-    initialize(){
-        windowMatchMedia.addEventListener("change", ()=>{ this.updateSidebar(); });
-        this.sidebarCoverElement.addEventListener("click", ()=>{ this.closeSidebar(); });
-        this.updateSidebar();
-    }
+    class Sidebar {
 
-    openSidebar() {
-        if(this.sidebarIsOpen) return;
-        this.sidebarIsOpen = true;
+        // ALL THE CODE NEEDED TO UNDERSTAND SIDEBAR FUNCTIONALITY
 
-        // animate the sidebar opening
+        initialize(){
+            this.displayScreen.addEventListener("change", ()=>{ this.updateSidebar(); });
+            this.sidebarToggleElement.addEventListener("click", ()=>{ this.toggleSideBar(); });
+            this.sidebarCoverElement.addEventListener("click", ()=>{ this.closeSidebar(); });
+            this.updateSidebar();
+        }
 
-        this.updateSidebar();
-    }
+        toggleSideBar() {
+            if(this.sidebarIsOpen) {
+                this.closeSidebar();
+            } else {
+                this.openSidebar();
+            }
+        }
 
-    closeSidebar() {
-        if(this.sidebarIsNotOpen()) return;
-        this.sidebarIsOpen = false;
+        openSidebar() {
+            if(this.sidebarIsOpen) return;
+            this.sidebarIsOpen = true;
+            this.showSidebar();
+            this.updateSidebar();
+        }
 
-        // animate the sidebar closing
-        this.updateSidebar();
-    }
+        closeSidebar() {
+            if(this.sidebarIsNotOpen()) return;
+            this.sidebarIsOpen = false;
+            this.hideSidebar();
+            this.updateSidebar(); 
+        }
 
-    updateSidebar() {
-        if(display.isSmall() && this.sidebarIsOpen) {
-            this.showSidebarCover();
-        } else {
-            this.hideSidebarCover();
+        updateSidebar() {
+            if(this.Display.isSmall()) {
+                if(this.sidebarIsOpen) {
+                    this.showSidebarCover();
+                } else {
+                    this.hideSidebarCover();
+                }
+            } else {
+                this.openSidebar();
+                this.hideSidebarCover();
+            }
+        }
+
+        // ALL THE CODE NEEDED TO UNDERSTAND SIDEBAR FUNCTIONALITY
+
+        /* EXTRA CODE YOU DO NOT NEED TO KNOW, all the mess is here
+                        |
+                        |
+                        |
+                        |
+                        |
+                        V
+        */
+        constructor(sidebarElement, sidebarToggleElement, sidebarCoverElement, displayScreen) {
+            this.sidebarElement = sidebarElement;
+            this.sidebarToggleElement = sidebarToggleElement;
+            this.sidebarCoverElement = sidebarCoverElement;
+            this.displayScreen = displayScreen;
+            this.sidebarIsOpen = true;
+            this.Display = {
+                isSmall: () => { return displayScreen.matches; }
+            }
+            this.initialize();
+        }
+
+        showSidebar(){
+            this.sidebarElement.classList.remove("custom-sidebar-closed");
+        }
+
+        hideSidebar(){
+            this.sidebarElement.classList.remove("custom-sidebar-closed");
+            this.sidebarElement.classList.add("custom-sidebar-closed");
+        }
+
+        showSidebarToggle() {
+            this.sidebarToggleElement.classList.remove("d-none");
+        }
+
+        hideSidebarToggle() {
+            this.sidebarToggleElement.classList.remove("d-none");
+            this.sidebarToggleElement.classList.add("d-none");
+        }
+
+        showSidebarCover(){
+            this.sidebarCoverElement.classList.remove("d-none");
+        }
+
+        hideSidebarCover(){
+            this.sidebarCoverElement.classList.remove("d-none");
+            this.sidebarCoverElement.classList.add("d-none");
+        }
+
+        sidebarIsNotOpen() {
+            return !this.sidebarIsOpen;
         }
     }
 
-    // ALL THE CODE NEEDED TO UNDERSTAND SIDEBAR FUNCTIONALITY
-
-    /* EXTRA CODE YOU DO NOT NEED TO KNOW
-                    |
-                    |
-                    |
-                    |
-                    |
-                    V
-    */
-
-    showSidebarCover(){
-        this.sidebarCoverElement.classList.remove("d-none");
+    {
+        const sidebarElement = document.querySelector(sidebarElementId);
+        const sidebarToggleElement = document.querySelector(sidebarToggleElementId);
+        const sidebarCoverElement = document.querySelector(sidebarCoverElementId);
+        const windowMatchMedia = window.matchMedia(`(max-width: ${smallDisplayWidth})`);
+        if(window.attachEvent) {
+            document.attachEvent("DOMContentLoaded", () => { 
+                sidebar = new Sidebar(sidebarElement, sidebarToggleElement, sidebarCoverElement, windowMatchMedia); 
+            } );
+        } else if (window.addEventListener) {
+            document.addEventListener("DOMContentLoaded", () => { 
+                sidebar = new Sidebar(sidebarElement, sidebarToggleElement, sidebarCoverElement, windowMatchMedia); 
+            } );
+        } else {
+            console.error("It seems that your browser doesn't support event listeners.");
+        }
     }
-
-    hideSidebarCover(){
-        this.sidebarCoverElement.classList.remove("d-none");
-        this.sidebarCoverElement.classList.add("d-none");
-    }
-
-    sidebarIsNotOpen() {
-        return !this.sidebarIsOpen;
-    }
-}
-
-if(window.attachEvent) {
-    document.attachEvent("DOMContentLoaded", () => { sidebar = new Sidebar(sidebarElement, sidebarCoverElement); } );
-} else if (window.addEventListener) {
-    document.addEventListener("DOMContentLoaded", () => { sidebar = new Sidebar(sidebarElement, sidebarCoverElement); } );
-} else {
-    console.error("It seems that your browser doesn't support event listeners.");
-}
