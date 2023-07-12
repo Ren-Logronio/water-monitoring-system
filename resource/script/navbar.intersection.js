b73e85dc42f189ab6b1e6dc7ff366f586d023173
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2023-07-13 04:43:17 +0800

    dashboard

--- resource/script/navbar.intersection.js
+++ resource/script/navbar.intersection.js
@@ -1,9 +1,36 @@
+const navbar = document.getElementById('navbar');
+const navbarLimit = 0.0;
+const scrollbarLimit = 0.0 ;
+const Scrollbar = {
+    hidden: 'transparent',
+    visible: document.documentElement.style.getPropertyValue('--scrollbar-color')
+};
 const options = {
-    root: document.querySelector('#intersection'),
+    root: null,
     rootMargin: '0px',
-    threshold: 0.5
+    threshold: []
+};
+
+for (let i = 0; i <= 1.0; i += 0.01) {
+    options.threshold.push(i);
 }
 
-const observer = new IntersectionObserver(() => {
-    console.log('IntersectionObserver');
-}, options);
+const observer = new IntersectionObserver(
+    (entries, observer) => {
+        entries.forEach(entry => {
+            if(entry.intersectionRatio <= navbarLimit) {
+                navbar.classList.remove('navbar-topped');
+                navbar.classList.add('navbar-scrolled');
+            } else {
+                navbar.classList.remove('navbar-scrolled');
+                navbar.classList.add('navbar-topped');
+            }
+            if (entry.intersectionRatio > scrollbarLimit) {
+                document.documentElement.style.setProperty('--scrollbar-color', Scrollbar.hidden);
+            } else {
+                document.documentElement.style.setProperty('--scrollbar-color', Scrollbar.visible);
+            }
+        });
+    }, options
+);
+observer.observe(document.getElementById('intersection'));
