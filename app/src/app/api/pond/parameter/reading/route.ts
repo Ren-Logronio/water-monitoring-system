11da4ba046c72b87bad52deb01c80da171c2c1c0
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-03-08 15:47:21 +0800

    fixed a bug where an entered parameter is not registered

---
+++ app/src/app/api/pond/parameter/reading/route.ts
@@ -0,0 +1,34 @@
+import getMySQLConnection from "@/db/mysql";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function GET(request: NextRequest) {
+    try {
+        const pond_id = request.nextUrl.searchParams.get('pond_id');
+        const parameter = request.nextUrl.searchParams.get('parameter');
+        const connection = await getMySQLConnection();
+        const [res, row] = await connection.query(
+            "SELECT * FROM `parameter_list` WHERE `name` = ?",
+            [parameter]
+        );
+        const [results, rows]: [results: any[], rows: any[]] = await connection.query(
+            "SELECT * FROM `view_pond_parameter_readings` WHERE `pond_id` = ? AND `parameter` = ?",
+            [pond_id, res[0].parameter]
+        );
+        console.log(results);
+        return NextResponse.json(
+            { results },
+            {
+                status: 200,
+            },
+        );
+    } catch (e) {
+        console.log(e);
+        return NextResponse.json(
+            { message: "Something went wrong while getting the readings info" },
+            {
+                status: 500,
+            },
+        );
+    }
+}
