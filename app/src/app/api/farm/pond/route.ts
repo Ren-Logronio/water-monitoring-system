2f2f0e0b5a213e298aea90767c071f753327e71b
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-03-08 15:47:47 +0800

    fixed a bug where the resulting pond_id are duplicates

--- app/src/app/api/farm/pond/route.ts
+++ app/src/app/api/farm/pond/route.ts
@@ -1,14 +1,18 @@
 import getMySQLConnection from "@/db/mysql";
+import getUserInfo from "@/utils/User";
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 
 export async function GET(request: NextRequest) {
     try {
         const farm_id = request.nextUrl.searchParams.get('farm_id');
+        const cookieToken = cookies().get('token')?.value;
+        const { user_id } = await getUserInfo(cookieToken);
         const connection = await getMySQLConnection();
         const [results, rows]: [results: any[], rows: any[]] = await connection.query(
-            "SELECT * FROM `view_farmer_ponds` WHERE `farm_id` = ?",
-            [farm_id]
+            "SELECT * FROM `view_farmer_ponds` WHERE `farm_id` = ? AND `user_id` = ?",
+            [farm_id, user_id]
         );
         return NextResponse.json(
             { results },
