80570a5fdbfce2d94bf22aaa5d26ec919127e26d
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-04-16 10:36:00 +0800

    improved data reading

--- resource/arduino-node-data-reader/index.ts
+++ resource/arduino-node-data-reader/index.ts
@@ -1,19 +1,39 @@
 import { ReadlineParser, SerialPort } from "serialport";
+import { ReadingSchema, readingModel } from "./model/readings";
+import fs from "fs";
 
 function readArduino() {
   SerialPort.list().then((ports) => {
     const arduinoPortInfo = ports.find((port) => port.manufacturer?.toLowerCase().startsWith("arduino"));
     if (arduinoPortInfo) {
-        const arduinoPort = new SerialPort({ path: arduinoPortInfo.path, baudRate: 9600 });
-        const parser = arduinoPort.pipe(new ReadlineParser({ delimiter: '\r\n' }));
-        parser.on("data", (data) => {
-            const parsedData = JSON.parse(data);
-            console.log(typeOfEachObjectKeys(parsedData));
-            console.log(data);
-        });
-        parser.on("error", (error) => {
+      const arduinoPort = new SerialPort({ path: arduinoPortInfo.path, baudRate: 9600 });
+      const parser = arduinoPort.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+      parser.on("data", (data: any) => {
+        const parsedData = JSON.parse(data);
+        console.log(typeOfEachObjectKeys(parsedData));
+        console.log(data);
+        // write to sensor.log file located at this directory
+        fs.appendFile("sensor.log", `${data},\n`, (error) => {
+          if (error) {
             console.error(error);
+          }
         });
+        // create new readings document
+        const newReading = new readingModel({
+          deviceId: parsedData.deviceId,
+          temperature: parsedData.temperature,
+          tds: parsedData.tds,
+          ph: parsedData.ph,
+          ammonia: parsedData.ammonia,
+        });
+        newReading.save().then((doc) => {
+        }).catch((error) => {
+          console.error(error);
+        });
+      });
+      parser.on("error", (error: any) => {
+        console.error(error);
+      });
     } else {
       console.error("Arduino not found");
     }
---
+++ resource/arduino-node-data-reader/lib/mongodb.ts
@@ -0,0 +1,3 @@
+import mongoose from "mongoose";
+
+const connection = mongoose.connect("mongodb+srv://Ren-logronio:QJcy2TU1Udi9z9oN@cluster0.46h8obk.mongodb.net/water-monitoring-system-db");
