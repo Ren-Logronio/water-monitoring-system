acaf0820d903fc605f1261a93458137b14700a9f
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-04-17 12:01:51 +0800

    potential import fix

--- app/src/components/Farm/PondList.tsx
+++ app/src/components/Farm/PondList.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { useEffect, useState } from "react"
-import AddPondDialog from "../ui/dialog/AddPond.dialog";
-import PondOptions from "./PondOptions";
-import { NinetyRing } from "react-svg-spinners";
-import { Badge } from "../ui/badge";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip";
+import AddPond from "AddPond";
+import DeletePond from "DeletePond";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
 
 export default function PondList({ farm_id }: { farm_id: number }) {
     const [ponds, setPonds] = useState<any[]>([])
@@ -44,26 +44,31 @@ export default function PondList({ farm_id }: { farm_id: number }) {
                 {ponds.map((pond, idx) => {
                     return <div key={idx} className="flex flex-col border-2 rounded-xl p-4 h-fit select-none align-middle hover:border-orange-300 transition-all ease-in-out duration-100">
                         <div className="flex flex-row justify-between items-center">
-
-
-
-                            <div className="flex flex-row cursor-pointer items-center space-x-2">
-                                <div className={` size-2 rounded-full  ${pond.device_id && pond.status === "ACTIVE" && "bg-green-600"} ${pond.device_id && pond.status === "IDLE" && "bg-orange-500"} ${(!pond.device_id || pond.status === "INACTIVE") && "bg-slate-500"}`}></div>
-                                <p className=" font-medium">{pond.name}</p>
-                            </div>
-
-                            {/* dropdown options */}
-                            <PondOptions pond_id={pond.pond_id} deleteCallback={handleRemovePond} />
-                        </div>
-
-                        <div className="flex flex-col mt-1">
-                            <p className="text-sm">{pond.method[0].toUpperCase() + pond.method.slice(1).toLowerCase()} pond</p>
-
-                            <div className="flex flex-row mt-4">
-                                <Badge key={pond} variant={"default"} className="m-0 w-fit">
-                                    Status: {pond.status || "No Device"}
-                                </Badge>
-                            </div>
+                            <TooltipProvider>
+                                <Tooltip>
+                                    <TooltipTrigger asChild>
+                                        <div className="flex flex-row cursor-pointer items-center space-x-2">
+                                            <div className={` size-2 rounded-full  ${pond.device_id && pond.status === "ACTIVE" && "bg-green-600"} ${pond.device_id && pond.status === "IDLE" && "bg-orange-500"} ${(!pond.device_id || pond.status === "INACTIVE") && "bg-slate-500"}`}></div>
+                                            <p className=" font-medium">{pond.name}</p>
+                                        </div>
+                                    </TooltipTrigger>
+                                    <TooltipContent className=" bg-blue-950 ">
+                                        <p>Status: {pond.status || "No Device"}</p>
+                                    </TooltipContent>
+                                </Tooltip>
+                            </TooltipProvider>
+                            <DeletePond pond_id={pond.pond_id} deleteCallback={handleRemovePond} />
+                            {/* <DropdownMenu>
+                                <DropdownMenuTrigger>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4">
+                                        <path d="M8 2a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM8 6.5a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM9.5 12.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0Z" />
+                                    </svg>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent>
+                                    <DropdownMenuItem className="cursor-pointer">Edit</DropdownMenuItem>
+                                    
+                                </DropdownMenuContent>
+                            </DropdownMenu> */}
                         </div>
                     </div>
                 })}
