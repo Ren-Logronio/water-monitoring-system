44e780fc9226e72af1fea7ef008106f0df3fda23
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-04-21 08:25:27 +0800

    fixed mongodb not connecting

--- resource/arduino-node-data-reader/index.ts
+++ resource/arduino-node-data-reader/index.ts
@@ -1,7 +1,9 @@
 import { ReadlineParser, SerialPort } from "serialport";
 import { ReadingSchema, readingModel } from "./model/readings";
-import { connection } from "./lib/mongodb";
 import fs from "fs";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb+srv://Ren-logronio:QJcy2TU1Udi9z9oN@cluster0.46h8obk.mongodb.net/water-monitoring-system-db");
 
 function readArduino() {
   SerialPort.list().then((ports: any) => {
@@ -15,7 +17,7 @@ function readArduino() {
         console.log(typeOfEachObjectKeys(parsedData));
         console.log(data);
         // write to sensor.log file located at this directory
-        fs.appendFile("sensor.log", `${data},\n`, (error) => {
+        fs.appendFile("sensor.log", `${data},\n`, (error: any) => {
           if (error) {
             console.error(error);
           }
@@ -28,9 +30,9 @@ function readArduino() {
           ph: parsedData.ph,
           ammonia: parsedData.ammonia,
         });
-        newReading.save().then((doc) => {
+        newReading.save().then((doc: any) => {
           console.log("saved to db");
-        }).catch((error) => {
+        }).catch((error: any) => {
           console.error(error);
         });
       });
@@ -39,12 +41,46 @@ function readArduino() {
       });
     } else {
       console.error("Arduino not found");
+      if (ports.length > 0) {
+        console.log("Opting for the first found port...");
+        const arduinoPort = new SerialPort({ path: ports[0].path, baudRate: 9600 });
+        const parser = arduinoPort.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+      parser.on("data", (data: any) => {
+        const parsedData = JSON.parse(data);
+        console.log(typeOfEachObjectKeys(parsedData));
+        console.log(data);
+        // write to sensor.log file located at this directory
+        fs.appendFile("sensor.log", `${data},\n`, (error: any) => {
+          if (error) {
+            console.error(error);
+          }
+        });
+        // create new readings document
+        const newReading = new readingModel({
+          deviceId: parsedData.deviceId,
+          temperature: parsedData.temperature,
+          tds: parsedData.tds,
+          ph: parsedData.ph,
+          ammonia: parsedData.ammonia,
+        });
+        newReading.save().then((doc: any) => {
+          console.log("saved to db");
+        }).catch((error: any) => {
+          console.error(error);
+        });
+      });
+      parser.on("error", (error: any) => {
+        console.error(error);
+      });
+      } else {
+        console.error("No ports found");
+      }
     }
   });
-}
+};
 
 function typeOfEachObjectKeys(obj: any) {
   return Object.keys(obj).map((key) => `${key}: ${typeof obj[key]}`);
-}
+};
 
 readArduino();
