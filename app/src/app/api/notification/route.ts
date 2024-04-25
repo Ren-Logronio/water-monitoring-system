74b314077f72de8a8578671301b4f2befdc82e8d
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-04-25 10:08:44 +0800

    added patch endpoint to notifications

--- app/src/app/api/notification/route.ts
+++ app/src/app/api/notification/route.ts
@@ -30,3 +30,28 @@ export async function GET(request: NextRequest) {
         );
     }
 }
+
+export async function PATCH(request: NextRequest) {
+    try {
+        const { notification_id } = await request.json();
+        const connection = await getMySQLConnection();
+        const [results, rows]: [results: any[], rows: any[]] = await connection.query(
+            "UPDATE `user_notifications` SET `isRead` = 1, SET `read_at` = ? WHERE `notification_id` = ?",
+            [new Date(), notification_id]
+        );
+        return NextResponse.json(
+            { message: "Notification read"},
+            {
+                status: 200,
+            },
+        );
+    } catch (err: any) {
+        console.log(err)
+        return NextResponse.json(
+            { message: "Something went wrong while updating the notification" },
+            {
+                status: 500,
+            },
+        );
+    }
+}
