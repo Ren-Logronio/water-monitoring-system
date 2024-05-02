ca1bb5e41bc0364efce321ca5400895ddb85809c
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-05-02 08:43:54 +0800

    ..

--- resource/sensor-log-to-mysql/index.js
+++ resource/sensor-log-to-mysql/index.js
@@ -1,8 +1,11 @@
 // import json file
-const sensorData = require('./sensor-data.json');
+const sensorData = require('./water-monitoring-system-db.sensor-logs.json');
 
 // import mysql module
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
+
+// import moment module
+const moment = require('moment-timezone');
 
 async function runMigrate() {
     const pond_id = 1;
@@ -14,8 +17,23 @@ async function runMigrate() {
     }); 
 
     const [parameters] = await connection.query("SELECT * FROM `parameters` WHERE `pond_id` = ?", [pond_id]);
+
+    sensorData.forEach((data, index) => {
+        //if (index > 0) return;
+        ["PH", "AMN", "TDS", "TMP"].forEach(async (param) => {
+            const parameter_id = parameters.find(p => p.parameter === param).parameter_id;
+            await connection.query(
+                "INSERT INTO `readings` (`parameter_id`, `value`, `recorded_at`) VALUES (?, ?, ?)", 
+                [parameter_id, data[param], moment.tz(data.recordedAt.$date, "Asia/Manila").format()]
+            );
+        })
+        console.log("Data Keys:", Object.keys(data));
+        console.log("Recorded at:", moment.tz(data.recordedAt.$date, "Asia/Manila").format());
+    });
     console.log("SENSOR DATA LENGTH:", sensorData.length);
     console.log("PARAMETERS:", parameters);
 };
+
+runMigrate();
 
 // create connection
