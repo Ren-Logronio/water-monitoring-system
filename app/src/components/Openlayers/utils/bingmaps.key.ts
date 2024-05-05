fc8deaa1bd5f4c0b8340cb30843b1977260e46fc
johnn600 <johnrey.vilbar@msugensan.edu.ph>
2024-05-06 00:11:44 +0800

    updated the bing key

--- app/src/components/Openlayers/utils/bingmaps.key.ts
+++ app/src/components/Openlayers/utils/bingmaps.key.ts
@@ -1,3 +1,3 @@
 // Get your Bing Maps key at https://www.bingmapsportal.com/Application
 
-export const BING_API_KEY = 'your key here';
+export const BING_API_KEY = process.env.NEXT_PUBLIC_BING_API_KEY; // replace with your Bing Maps key
