60f78341b7451e18e89e9ff5ba313e701062eb5c
johnn600 <johnrey.vilbar@msugensan.edu.ph>
2024-05-07 16:35:14 +0800

    updated pondGeometryStore

--- app/src/store/pondGeometryStore.ts
+++ app/src/store/pondGeometryStore.ts
@@ -4,12 +4,16 @@ import { create } from "zustand";
 
 interface PondGeometryStore {
     ponds: Feature[];
+    labels: Feature[];
     storePonds: (ponds: Feature[]) => void;
+    storeLabels: (labels: Feature[]) => void;
+    updateLabels: (id: number, label: string) => void;
 }
 
 
 export const usePondGeometryStore = create<PondGeometryStore>((set) => ({
     ponds: [],
+    labels: [],
 
     // store the ponds
     storePonds: (ponds: Feature[]) => { 
@@ -17,6 +21,29 @@ export const usePondGeometryStore = create<PondGeometryStore>((set) => ({
             return {
                 ponds: [...state.ponds, ...ponds], 
             };
+        });
+    },
+
+    // store the labels
+    storeLabels: (labels: Feature[]) => {
+        set((state) => {
+            return {
+                labels: [...state.labels, ...labels],
+            };
+        });
+    },
+
+    // update the labels by id
+    updateLabels: (id: number, label: string) => {
+        set((state) => {
+            // find the feature by id
+            let feature = state.labels.find((feature) => feature.getId() === id);
+            
+            // update the label
+            if (feature) {
+                feature.set("name", label);
+            }
+            return state;
         });
     },
 }));
