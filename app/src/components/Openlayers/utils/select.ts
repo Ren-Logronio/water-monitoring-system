dc9f6d38a5313b5a976b366bc2dae04007ed889d
johnn600 <johnrey.vilbar@msugensan.edu.ph>
2024-05-07 16:34:12 +0800

    revised select.ts to have callable methods

--- app/src/components/Openlayers/utils/select.ts
+++ app/src/components/Openlayers/utils/select.ts
@@ -1,32 +1,57 @@
 // select interaction
 import { Select } from 'ol/interaction';
 import { click } from 'ol/events/condition';
-import { vectorLayer } from './vectorSource';
+import VectorLayer from 'ol/layer/Vector';
 
 // style for selected/unselected feature
 import { styleSelected, styleNormal } from './vectorStyle';
+import Feature from 'ol/Feature';
 
 
-export const selectInteraction = () => {
+// select interaction, needs the vector layer as parameter
+// to ensure that each map instance has its own select interaction
+export const selectInteraction = (handleFeatureSelection: (feature: Feature<any> | null) => void) => {
+    
+    // holds the selected feature
+    let selectedFeature: Feature<any> | null = null;
 
-    // create the select interaction
-    const select = new Select({
-        condition: click,
-        layers: [vectorLayer],
-    });
-
-    // on select change set the style of the selected feature
-    select.on('select', (e) => {
-        // set the style of the selected feature
-        e.selected.forEach((feature) => {
-            feature.setStyle(styleSelected);
+    function newSelect(vectorLayer: VectorLayer<any>) {
+        // create the select interaction
+        const select = new Select({
+            condition: click,
+            layers: [vectorLayer],
         });
 
-        // set the style of the unselected feature
-        e.deselected.forEach((feature) => {
-            feature.setStyle(styleNormal);
-        });
-    });
+        // on select change set the style of the selected feature
+        select.on('select', (e) => {
+            // when a feature is selected
+            e.selected.forEach((feature) => {
+                feature.setStyle(styleSelected);
+                selectedFeature = feature;
+                
+                // handle the selected feature
+                handleFeatureSelection(selectedFeature);
+            });
 
-    return select;
+            // when a feature is deselected
+            e.deselected.forEach((feature) => {
+                feature.setStyle(styleNormal);
+                selectedFeature = null;
+            });
+
+        });
+        
+
+        return select;
+        }
+
+    function getSelectedFeature() {
+        return selectedFeature;
+    }
+
+    // return the methods
+    return {
+        newSelect: newSelect,
+        selectedFeature: getSelectedFeature,
+    }
 }
