d166605e3a007ee909f10dcb1088ec2ac978e75a
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-05-08 16:07:59 +0800

    added pagination, no of items, date range to logs

--- app/src/components/ParameterDatasheet/index.tsx
+++ app/src/components/ParameterDatasheet/index.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { useParams, useSearchParams } from "next/navigation";
-import { format } from "date-fns";
-import { Dispatch, SetStateAction, use, useEffect, useRef, useState } from "react";
+import { useParams, usePathname, useRouter, useSearchParams } from "next/navigation";
+import { format, set } from "date-fns";
+import { Dispatch, SetStateAction, use, useEffect, useMemo, useRef, useState } from "react";
 import { AgGridReact } from 'ag-grid-react';
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-material.css";
@@ -19,6 +19,11 @@ import Actions from "./Actions";
 import { useParameterDatasheetStore } from "@/store/parameterDatasheetStore";
 import { PopoverContent, PopoverTrigger, Popover } from "../ui/popover";
 import DaterangePopover from "../ui/daterange";
+import NumberOfItemsDropdown, { DefaultValueLabelSet } from "../ui/no-of-items-dropdown";
+import moment from "moment";
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "../ui/pagination";
+import useAuth from "@/hooks/useAuth";
+import Aggregation, { DefaultAggregationValueLabelSet } from "../ui/aggregation";
 
 const autoSizeStrategy: any = {
     type: 'fitGridWidth',
@@ -37,8 +42,15 @@ const autoSizeStrategy: any = {
 
 
 export default function ParameterDatasheet({ pond_id, setSelectedPond, ponds, pondsLoading }: { pond_id?: string, ponds?: any[], pondsLoading: boolean, setSelectedPond: Dispatch<SetStateAction<string>> }) {
-    // get parameter from the url
+    const { user } = useAuth();
+    const [selected, setSelected] = useState<string>("all");
+    const [dateFrom, setDateFrom] = useState<Date>(moment.unix(0).toDate());
+    const [dateTo, setDateTo] = useState<Date>(moment().toDate());
+    const [aggregation, setAggregation] = useState<string>("4");
+    const searchParams = useSearchParams();
     const params = useParams();
+    const router = useRouter();
+    const pathname = usePathname();
     const { rowData, setRowData, purgeRowData } = useParameterDatasheetStore();
     const inputFile = useRef<HTMLInputElement | null>(null);
     const [loading, setLoading] = useState(true);
@@ -50,9 +62,30 @@ export default function ParameterDatasheet({ pond_id, setSelectedPond, ponds, po
         { field: "reading", headerName: (params.parameter && `${params.parameter[0].toUpperCase()}${params.parameter.slice(1)}`) || "Reading", lockPosition: "left", resizable: false },
         { field: "date", headerName: "Date", lockPosition: "left", resizable: false },
         { field: "time", headerName: "Time", lockPosition: "left", resizable: false },
-        { field: "recorded_by", headerName: "Recorded By", lockPosition: "left", resizable: false },
+        // { field: "recorded_by", headerName: "Recorded By", lockPosition: "left", resizable: false },
         { headerName: "Actions", lockPosition: "right", cellRenderer: Actions, valueGetter: (params: any) => ({ reading_id: params.data.reading_id, reading: params.data.reading, date: params.data.edit_recorded_at, time: params.data.edit_time }), resizable: false }
     ]);
+
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const numberOfPages = useMemo(() => {
+        const items = searchParams.get("items") === "all" ? 99999 : parseInt(searchParams.get("items") || "25");
+        return Math.ceil(rowData.length / items);
+    }, [rowData, searchParams.get("items")])
+
+    const filteredDate = useMemo(() => {
+        const filterDateRange = rowData.filter((row: any) => {
+            const date = moment(row.edit_recorded_at);
+            return date.isBetween(dateFrom, dateTo, "day", "[]");
+        });
+        console.log("filteredDate:", filterDateRange);
+        return filterDateRange;
+    }, [rowData, dateFrom, dateTo, selected]);
+
+    const paginatedItems = useMemo(() => {
+        const items = searchParams.get("items") === "all" ? 99999 : parseInt(searchParams.get("items") || "25");
+        return filteredDate.slice((currentPage - 1) * items, currentPage * items);
+    }, [filteredDate, searchParams.get("items"), currentPage]); 
 
     useEffect(() => {
         !loading && setLoading(true);
@@ -60,7 +93,21 @@ export default function ParameterDatasheet({ pond_id, setSelectedPond, ponds, po
         axios.get(`/api/pond/parameter/reading?pond_id=${pond_id}&parameter=${params.parameter}`).then(response => {
             if (response.data.results && response.data.results.length > 0) {
                 console.log("response.data.results:", response.data.results);
-                setRowData(response.data.results.sort((a: any, b: any) => b.reading_id - a.reading_id).map((row: any, idx: number) => ({ idx: idx + 1, reading_id: row.reading_id, edit_recorded_at: format(row.recorded_at, "yyyy-MM-dd"), edit_time: format(row.recorded_at, "hh:mm"), reading: `${row.value} ${row.unit}`, date: format(new Date(row.recorded_at), "MMM dd, yyyy"), time: format(new Date(row.recorded_at), "h:mm a"), recorded_by: row.isRecordedBySensor ? "sensor" : "farmer" })));
+                setRowData(response.data.results
+                    .sort((a: any, b: any) => b.reading_id - a.reading_id)
+                    .map((row: any, idx: number) => ({ 
+                        idx: idx + 1, 
+                        reading_id: row.reading_id, 
+                        edit_recorded_at: format(row.recorded_at, "yyyy-MM-dd"), 
+                        edit_time: format(row.recorded_at, "hh:mm"), 
+                        reading: `${row.value} ${row.unit}`, 
+                        date: format(new Date(row.recorded_at), "MMM dd, yyyy"), 
+                        time: format(new Date(row.recorded_at), "h:mm a"), 
+                        recorded_at: row.recorded_at,
+                        // recorded_by: row.isRecordedBySensor ? "sensor" : "farmer" 
+                    })
+                    )
+                );
             }
         }).catch(error => {
             console.error(error);
@@ -104,7 +151,7 @@ export default function ParameterDatasheet({ pond_id, setSelectedPond, ponds, po
                                     </Select>
                                 </>
                             }
-                            <AddReading pond_id={pond_id} />
+                            {/* <AddReading pond_id={pond_id} /> */}
                             {/* <Button variant="outline" onClick={handleFileImportPress} className="flex flex-row space-x-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                                     <path d="M9.25 13.25a.75.75 0 0 0 1.5 0V4.636l2.955 3.129a.75.75 0 0 0 1.09-1.03l-4.25-4.5a.75.75 0 0 0-1.09 0l-4.25 4.5a.75.75 0 1 0 1.09 1.03L9.25 4.636v8.614Z" />
@@ -114,19 +161,59 @@ export default function ParameterDatasheet({ pond_id, setSelectedPond, ponds, po
                             </Button> */}
                             <Input type="file" ref={inputFile} accept=".csv" className="hidden" />
                         </div>
-                        <div className="flex flex-row space-x-2">
+                        <div className="flex flex-row space-x-2 text-[14px]">
                             {/* <Button className="flex flex-row space-x-2" variant="outline">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                                     <path fillRule="evenodd" d="M5 2.75C5 1.784 5.784 1 6.75 1h6.5c.966 0 1.75.784 1.75 1.75v3.552c.377.046.752.097 1.126.153A2.212 2.212 0 0 1 18 8.653v4.097A2.25 2.25 0 0 1 15.75 15h-.241l.305 1.984A1.75 1.75 0 0 1 14.084 19H5.915a1.75 1.75 0 0 1-1.73-2.016L4.492 15H4.25A2.25 2.25 0 0 1 2 12.75V8.653c0-1.082.775-2.034 1.874-2.198.374-.056.75-.107 1.127-.153L5 6.25v-3.5Zm8.5 3.397a41.533 41.533 0 0 0-7 0V2.75a.25.25 0 0 1 .25-.25h6.5a.25.25 0 0 1 .25.25v3.397ZM6.608 12.5a.25.25 0 0 0-.247.212l-.693 4.5a.25.25 0 0 0 .247.288h8.17a.25.25 0 0 0 .246-.288l-.692-4.5a.25.25 0 0 0-.247-.212H6.608Z" clipRule="evenodd" />
                                 </svg>
                                 <p>Print View</p>
                             </Button> */}
-                            <DaterangePopover />
+                            <div className="flex flex-row items-center space-x-2 relative">
+                                <NumberOfItemsDropdown 
+                                    setValueLabel={DefaultValueLabelSet} 
+                                    defaultValue={searchParams.get("items") || "25"}
+                                    onValueChange={(value) => {
+                                        setCurrentPage(1);
+                                        router.push(`${pathname}?items=${value}`);
+                                    }} />
+                            </div>
+                            <div className="flex flex-row items-center space-x-2 relative">
+                                <DaterangePopover onChange={(dateFrom, dateTo, mode) => {
+                                    setDateFrom(dateFrom);
+                                    setDateTo(dateTo);
+                                    setSelected(mode);
+                                }}/>
+                            </div>
+                            <Aggregation setValueLabel={DefaultAggregationValueLabelSet} defaultValue="minute" onValueChange={() => {
+                                setCurrentPage(1);
+                                setAggregation(aggregation);
+                            }} />
                             <Download pond_id={pond_id} />
                         </div>
                     </div>
                     <div className="ag-theme-material h-[calc(100vh-200px)]">
-                        <AgGridReact rowData={rowData} columnDefs={columnDefs} autoSizeStrategy={autoSizeStrategy} />
+                        <AgGridReact rowData={paginatedItems} columnDefs={columnDefs} autoSizeStrategy={autoSizeStrategy} />
+                        { !!numberOfPages && numberOfPages !== 1 && <Pagination className="mt-2">
+                            <PaginationContent>
+                                {currentPage > 1 && <PaginationItem>
+                                    <PaginationPrevious className=" cursor-pointer" onClick={ () => setCurrentPage(prev => prev - 1)} />
+                                </PaginationItem>}
+                                {
+                                    Array.from({ length: numberOfPages }, (_, idx) => idx + 1).map((page) => {
+                                        return <>{
+                                        (page === currentPage || (page < (currentPage + 3) && page > (currentPage - 3))) && <PaginationItem key={page}>
+                                            <PaginationLink className=" cursor-pointer" onClick={() => { setCurrentPage(page) }} isActive={currentPage === page}>
+                                                {page}
+                                            </PaginationLink>
+                                        </PaginationItem>
+                                        }</>
+                                    })
+                                }
+                                {!(currentPage > (numberOfPages - 1)) && <PaginationItem>
+                                <PaginationNext className=" cursor-pointer" onClick={ () => setCurrentPage(prev => prev + 1) } />
+                                </PaginationItem>}
+                            </PaginationContent>
+                        </Pagination>}
                     </div>
                 </>
             }
---
+++ app/src/components/ui/no-of-items-dropdown.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { Button } from "./button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuSeparator, DropdownMenuTrigger } from "./dropdown-menu";
+
+type ValueLabelType = {
+    value: string;
+    label: string;
+}
+
+type NumberOfItemsDropdownProps = {
+    setValueLabel: ValueLabelType[];
+    defaultValue?: string;
+    onValueChange?: (value: string) => void;
+}
+
+export const DefaultValueLabelSet = [
+    { value: "25", label: "25" },
+    { value: "75", label: "75" },
+    { value: "150", label: "150" },
+    { value: "all", label: "Show All" },
+]
+
+export default function NumberOfItemsDropdown({ setValueLabel, defaultValue, onValueChange }: NumberOfItemsDropdownProps) {
+    const [value, setValue] = useState<string>(defaultValue || setValueLabel[0].value);
+
+    useEffect(() => {
+        onValueChange && onValueChange(value);
+    }, [value]);
+    
+    return <DropdownMenu>
+    <DropdownMenuTrigger className="border py-1 px-3 bg-white rounded-md flex flex-row items-center space-x-2 text-[14px]">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+            <path fillRule="evenodd" d="M2.625 6.75a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875 0A.75.75 0 0 1 8.25 6h12a.75.75 0 0 1 0 1.5h-12a.75.75 0 0 1-.75-.75ZM2.625 12a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0ZM7.5 12a.75.75 0 0 1 .75-.75h12a.75.75 0 0 1 0 1.5h-12A.75.75 0 0 1 7.5 12Zm-4.875 5.25a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875 0a.75.75 0 0 1 .75-.75h12a.75.75 0 0 1 0 1.5h-12a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
+        </svg>
+        <span>Show {value} Items</span>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent className="w-56">
+      <DropdownMenuRadioGroup value={value} onValueChange={setValue}>
+        {setValueLabel.map(({ value, label }) => <DropdownMenuRadioItem key={value} value={value}>{label}</DropdownMenuRadioItem>)}
+      </DropdownMenuRadioGroup>
+    </DropdownMenuContent>
+  </DropdownMenu>
+}
