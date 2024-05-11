ee15fd62f3689f94f0663d074fd4905126cad9ce
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-05-11 16:24:13 +0800

    addde hardcoded thresholds

---
+++ app/src/utils/HardcodedThresholds.tsx
@@ -0,0 +1,15 @@
+
+export const hardcodedThresholds = {
+    "TMP": {
+        min: 20, max: 30, unit: "°C", name: "Temperature"
+    },
+    "TDS": {
+        min: Number.NEGATIVE_INFINITY, max: 600, unit: "ppm", name: "Total Dissolved Solids"
+    },
+    "AMN": {
+        min: Number.NEGATIVE_INFINITY, max: 300, unit: "ppm", name: "Ammonia"
+    },
+    "PH": {
+        min: 6, max: 9, unit: "", name: "pH"
+    },
+};
