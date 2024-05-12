52dc45a7a2fbd0ca1e0f96dab2bea259a45b1042
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-05-12 18:18:52 +0800

    change Every Date to All Dates

--- app/src/components/ui/daterange.tsx
+++ app/src/components/ui/daterange.tsx
@@ -6,6 +6,7 @@ import moment from "moment";
 import { Label } from "./label";
 import { Button } from "./button";
 import { DateRange } from "react-day-picker";
+import { Separator } from "./separator";
 
 export default function DaterangePopover({ children, asChild = false, onChange, disabled = false }: { disabled?: boolean, children?: React.ReactNode, asChild?: boolean, onChange?: (dateFrom: Date, dateTo: Date, mode: string) => void }) {
     const [selected, setSelected] = useState<string>("all");
@@ -40,16 +41,17 @@ export default function DaterangePopover({ children, asChild = false, onChange, 
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
                 </svg>
+                <span>Date Range</span>
+                <span className="text-[11px]">-</span>
                 { selected === "custom" && <span>
                     {!dateFrom && !dateTo && "Choose Date Range"}
                     {dateFrom && !moment(dateFrom).isSame(moment(dateTo)) && !moment(dateFrom).isSame(moment.unix(0)) && `${moment(dateFrom).format("MMM DD, YYYY")} --`}
                     {dateFrom && moment(dateFrom).isSame(moment.unix(0)) && "Until"}
-                    {dateTo && !moment(dateTo).isSame(dateTo, "day") && ` ${moment(dateTo).format("MMM DD, YYYY")}`}
-                    {dateTo && moment(dateTo).isSame(dateTo, "day") && ` Today`}
+                    {dateTo  && ` ${moment(dateTo).format("MMM DD, YYYY")}`}
                 </span>}
                 {
                     selected !== "custom" && <span>
-                        {selected === "all" && "Every date"}
+                        {selected === "all" && "All Dates"}
                         {selected === "30" && "Last 30 days"}
                         {selected === "7" && "Last 7 days"}
                         {selected === "24h" && "Last 24 hours"}
